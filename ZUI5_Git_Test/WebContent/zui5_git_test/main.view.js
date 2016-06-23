a1d787be89623c02b341325cd8c34bf0f02fd7d5
Khabir Ahmad Raja <khabir.raja@2bits.de>
2016-06-23 17:50:10 +0200

    View comes back again

---
+++ ZUI5_Git_Test/WebContent/zui5_git_test/main.view.js
@@ -0,0 +1,26 @@
+sap.ui.jsview("zui5_git_test.main", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf zui5_git_test.main
+	*/ 
+	getControllerName : function() {
+		return "zui5_git_test.main";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf zui5_git_test.main
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Git Test",
+			content: [
+			          new sap.m.Text({text: "Hello!"}),
+			          
+			          new sap.m.Text({text: "Hello!"}),
+			]
+		});
+	}
+
+});
