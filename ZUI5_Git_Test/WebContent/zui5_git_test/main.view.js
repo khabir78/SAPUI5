79d2550691037709bdc9ed9206f22dbf8476e62c
Khabir Ahmad Raja <khabir.raja@2bits.de>
2016-06-24 09:05:08 +0200

    Kommentar Erweiterung 2

--- ZUI5_Git_Test/WebContent/zui5_git_test/main.view.js
+++ ZUI5_Git_Test/WebContent/zui5_git_test/main.view.js
@@ -20,7 +20,8 @@ sap.ui.jsview("zui5_git_test.main", {
 			          
 			          new sap.m.Text({text: "Hello!"}),
 			          
-			          //  Test Kommentar 
+			          //  Test Kommentar Heir kann mann viel shcreiben
+			          new sap.m.Text({text: "Felix"}),
 			]
 		});
 	}
