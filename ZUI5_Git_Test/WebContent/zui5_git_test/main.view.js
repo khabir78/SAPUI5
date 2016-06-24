8b1f37a1c5d01d0cd4fe0a81b957fec8c224aef1
Felix Nenninger <felix.nenninger@2bits.de>
2016-06-24 17:17:08 +0200

    Double

--- ZUI5_Git_Test/WebContent/zui5_git_test/main.view.js
+++ ZUI5_Git_Test/WebContent/zui5_git_test/main.view.js
@@ -25,7 +25,10 @@ sap.ui.jsview("zui5_git_test.main", {
 
 			          new sap.m.Label({text: "This is a Label"}),			         
 
+			          new sap.m.Text({text: "Hello!"}),  
+			          
 
+			          new sap.m.Label({text: "This is a Label"}),	
 			          
 			          
 
