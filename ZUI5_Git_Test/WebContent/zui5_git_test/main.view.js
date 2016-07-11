eafedd09419a0bd1cf38e2962f74019416bb188c
Felix Nenninger <felixnenninger@gmail.com>
2016-07-11 15:25:00 +0000

    Delete test

--- ZUI5_Git_Test/WebContent/zui5_git_test/main.view.js
+++ ZUI5_Git_Test/WebContent/zui5_git_test/main.view.js
@@ -22,11 +22,6 @@ sap.ui.jsview("zui5_git_test.main", {
 
 			          new sap.m.Text({text: "Hello!"}),  			          
 
-
-			          new sap.m.Label({text: "This is a Label"}),
-
-			          new sap.m.Label({text: "This is a Label"}),			         
-
 			          new sap.m.Text({text: "Hello!"}),  			          
 
 			          new sap.m.Label({text: "This"}),
